b72a4ea1acdacc6a87173b48b2b004d7a07cf067
hdoherty2022 <49986402+hdoherty2022@users.noreply.github.com>
2019-05-23 09:42:00 -0400

    final push

--- js/slideshow.js
+++ js/slideshow.js
@@ -13,5 +13,28 @@ function openLink(evt, linkName) {
   evt.currentTarget.className += " w3-red";
 }
 
-// Click on the first tablink on load
-document.getElementsByClassName("tablink")[0].click();
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
